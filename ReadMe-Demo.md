2e777ebba2b480a2695d33dd97a6c6f045559a37
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-08-23 21:54:19 -0400

    Updating files.

---
+++ ReadMe-Demo.md
@@ -0,0 +1,40 @@
+# README Demo
+  ## Description
+  This is just a demonstration.
+
+  ## Table of Contents
+  * [Installation] (#Installation)
+  * [Usage] (#Usage)
+  * [Contributing] (#Contributing)
+  * [Tests] (#Tests)
+  * [Questions] (#Questions)
+  * [License] (#License)
+
+
+  ## Installation
+  This is just a demonstration, no installation instructions.
+
+  ## Usage
+  No usage instructions, this is just a demonstration.
+
+
+  ## Contributing
+  Just me!
+
+
+  ## Tests
+  No tests.
+
+
+  ## Questions
+  For questions about this repository: 
+
+  GitHub Username: kdunphe
+
+  Email Address: kayla.dunphe@gmail.com
+
+
+
+  ## License
+  The repository is using the MIT license.
+
--- index.js
+++ index.js
@@ -1,62 +1,113 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// array of questions for user
 const questions = [{
-    type: "input",
-    name: "Title",
-    message: "Project Title?",
+  type: "input",
+  name: "Title",
+  message: "Project Title?",
 
 },
 {
-    type: "input",
-    name: "Description",
-    message: "Project Description?",  
+  type: "input",
+  name: "Description",
+  message: "Project Description?",
 },
 {
   type: "input",
   name: "Installation",
-  message: "Add any Installation Instructions:",  
+  message: "Add any Installation Instructions:",
 },
 {
   type: "input",
   name: "Usage",
-  message: "Add any Usage Instructions:",  
+  message: "Add any Usage Instructions:",
 },
 {
   type: "input",
   name: "Contributing",
-  message: "Add any Contributors to this Project:",  
+  message: "Add any Contributors to this Project:",
 },
 {
   type: "input",
   name: "Tests",
-  message: "Add any Tests for your Project:",  
+  message: "Add any Tests for your Project:",
+},
+{
+  type: "input",
+  name: "License",
+  message: "Project License?",
 },
 {
   type: "input",
   name: "Username",
-  message: "What is your GitHub Username?",  
+  message: "What is your GitHub Username?",
 },
 {
   type: "input",
   name: "Email",
-  message: "What is your email address?",  
+  message: "What is your email address?",
 }
 ];
 
-function formatText (answers) {
-    let outputString = "";
-    outputString += `# ${answers.Title} \n`;
-    outputString += `# ${answers.Description} \n`;
-    return outputString;
+// function to write README file
+// function formatText (answers) {
+//     let outputString = "";
+//     outputString += `# ${answers.Title} \n`;
+//     outputString += `# ${answers.Description} \n`;
+//     return outputString;
+// }
 
+function formatText(answers) {
+  return `# ${answers.Title}
+  ## Description
+  ${answers.Description}
+
+  ## Table of Contents
+  * [Installation] (#Installation)
+  * [Usage] (#Usage)
+  * [Contributing] (#Contributing)
+  * [Tests] (#Tests)
+  * [Questions] (#Questions)
+  * [License] (#License)
+
+
+  ## Installation
+  ${answers.Installation}
+
+  ## Usage
+  ${answers.Usage}
+
+
+  ## Contributing
+  ${answers.Contributing}
+
+
+  ## Tests
+  ${answers.Tests}
+
+
+  ## Questions
+  For questions about this repository: \n
+  GitHub Username: ${answers.Username}\n
+  Email Address: ${answers.Email}\n
+
+
+  ## License
+  The repository is using the ${answers.License} license.
+
+`
 }
 
+
+
+
+// function to initialize program
 // Source: https://www.npmjs.com/package/inquirer
-function init () {
-    inquirer
-  .prompt(questions)
-  .then(answers => {
+function init() {
+  return inquirer
+    .prompt(questions)
+    .then(answers => {
       console.log(answers);
       // Format responses in markdown
       let markdown = formatText(answers);
@@ -65,11 +116,12 @@ function init () {
         if (err) throw err;
         console.log("Done! Successfully created ReadMe-Demo.md");
       });
-  })
+    })
 
-  .catch(err => {
-    console.log(err)
-  });
+    .catch(err => {
+      console.log(err)
+    });
 }
 
+// function call to initialize program
 init();
